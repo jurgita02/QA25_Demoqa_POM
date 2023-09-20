7a671cafe4acd7f2d4c5f8a42ab33703586a3a85
jurgita02 <jurgita02@yahoo.com>
2023-09-20 13:47:53 +0200

    added Classes

---
+++ src/main/java/org/ait/demoqa/pages/LoginPage.java
@@ -0,0 +1,42 @@
+package org.ait.demoqa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "userName")
+    WebElement userNameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "login")
+    WebElement loginButton;
+
+
+    public ProfilePage login(String userName, String password) {
+        type(userNameField, userName);
+        type(passwordField, password);
+        click(loginButton);
+        return new ProfilePage(driver);
+    }
+
+
+//sitas metodas rasomas, kaip vz pilna reklamu ant psl, ir neina Selenium paspausti knopkiu
+    //yra ten reklamu. Selenium kaip naudotojas yra, ir per issokancias reklamas negali ivest
+    //pas mane nera reklamu, del to as tik komentinu
+
+    public LoginPage loginNegative(String userName, String password) {
+        typeWithJSExecutor(userNameField, userName, 0, 200);
+        type(passwordField, password);
+        click(loginButton);
+        return this;
+    }
+
+}
--- src/test/java/org/ait/demoqa/tests/TestBase.java
+++ src/test/java/org/ait/demoqa/tests/TestBase.java
@@ -12,7 +12,7 @@ public class TestBase {
     @BeforeMethod
     public void init(){
         driver=new ChromeDriver();
-        driver.get("https://demoqa.com/");
+        driver.get("https://demoqa.com");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
